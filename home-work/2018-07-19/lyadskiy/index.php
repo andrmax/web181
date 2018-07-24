0e7c5d5d0d0dcb94e4461658d99c03eacd0ff5ec
Smart97 <smart9713@gmail.com>
2018-07-24 11:44:49 +0300

    Сделал ДЗ

---
+++ home-work/2018-07-19/lyadskiy/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Date: 19.07.18
+ * @author Isaenko Alexey <info@oiplug.com>
+ */
+
+require '../../../config.php';
+require 'functions.php';
+
+
+init();
+
+
+
+
+
+// eof
---
+++ home-work/2018-07-19/lyadskiy/sections/calculator.php
@@ -0,0 +1,22 @@
+<?php
+$number = 0;
+$result = '';
+if ( ! empty( $_GET['number'] ) ) {
+// определение первой цифры числа
+	$number = $_GET['number'];
+	$result = floor( $number / pow( 10, floor( log10( $number ) ) ) );
+	$result = 'Первая цифра числа ' . $number . ': ' . $result;
+
+}
+?>
+
+<div class="section-math">
+	<div class="section-math__container">
+		<form action="" class="section-math__form">
+			<input type="text" class="section-math__input" name="number" placeholder="введите число"
+			       value="<?php echo $number; ?>">
+			<button class="section-math__submit">ok</button>
+		</form>
+		<div class="section-math__result"><?php echo $result; ?></div>
+	</div>
+</div>
