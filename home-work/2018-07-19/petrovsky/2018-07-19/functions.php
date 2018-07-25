281f4815d83a91ad8beaf892cadd555d4901ccf6
Павел <atrosity@narod.ru>
2018-07-25 21:38:41 +0300

    домашка за 2018-07-19

---
+++ home-work/2018-07-19/petrovsky/2018-07-19/functions.php
@@ -0,0 +1,42 @@
+<?php
+
+function get_template( $adds ) {
+    $path = 'sections/' . $adds . '.php';
+    //print_r($path);
+    if ( file_exists( $path ) ) {
+        include $path;
+    }
+}
+
+function construct_page($sections){
+    foreach ($sections as $section){
+        get_template( $section );
+    }
+}
+
+
+function init() {
+    include 'templates/header.php';
+
+    $sections = array(
+        'top',
+        'calculator',
+        'call-to-action',
+        'bottom',
+    );
+
+    construct_page($sections);
+    include 'templates/footer.php';
+}
+
+function filter_mail()
+{
+    if ( ! empty( $_GET['email'] ) ) {
+        $email_a=$_GET['email'];
+        if (!(filter_var($email_a, FILTER_VALIDATE_EMAIL))) {
+            echo "E-mail адрес '$email_a' указан не верно.\n";
+        }
+    }
+
+
+}
---
+++ home-work/2018-07-19/petrovsky/2018-07-19/index.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: text/html; charset=utf-8', true);
+require '../config.php';
+require 'functions.php';
+
+
+init();
+
+
