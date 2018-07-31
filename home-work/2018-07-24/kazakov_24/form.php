a42f75fbb9888a25ee187d2b4073fa561d85b220
Kirill <Nordian89@mail.ru>
2018-07-31 04:17:37 +0300

    выполненные задачи: 1) Преобразовать дату из формата строки в формат timestamp, а timestamp в свою очередь в удобочитаемую дату (в формат даты) 2) Придать внеший вид 3) Сделать вывод не кодов json, а русских букв и слов (решается флагом) 4) Вернуть вид "Дата => заголовок => текст"

---
+++ home-work/2018-07-24/kazakov_24/form.php
@@ -0,0 +1,58 @@
+<?php
+
+$errors = save_post();
+$data   = array(
+	'title'   => '',
+	'content' => '',
+);
+if ( ! empty( $errors ) ) {
+	$data = $_POST;
+}
+echo $errors;
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Ниже подключены шрифты из GOOGLE -->
+    <link href="https://fonts.googleapis.com/css?family=Caveat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Pattaya" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Jura" rel="stylesheet">
+
+</head>
+<body>
+<div class="general">
+    <form method="post" class="form">
+        <div class="over_caption">
+            <div class="sign">Тема:</div>
+            <div class="caption">
+                <input class="sub_caption" type="text" name="title" value="<?php echo $data['title']; ?>">
+            </div>
+        </div>
+        <div class="over_message">
+            <div class="sign">Сообщение:</div>
+            <textarea class="sub_message" name="content" id="" cols="30" rows="10" class="form__textarea">
+                <?php echo $data['content']; ?>
+            </textarea>
+        </div>
+        <div class="button">
+            <button type="submit" class="form__submit">OK</button>
+        </div>
+        <input type="hidden" name="save_post" value="1">
+    </form>
+    <?php
+    get_posts();
+
+    ?>
+</div>
+
+
+</body>
+</html>
