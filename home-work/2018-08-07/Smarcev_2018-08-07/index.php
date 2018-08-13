ea2f4a66d2bebc60d60678f1221eb23a441affdc
mixdior <mixdior@gmail.com>
2018-08-14 00:12:52 +0300

    Домашнее задание от 07.08.2018: -навигация по страницам -без функции меню.

---
+++ home-work/2018-08-07/Smarcev_2018-08-07/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+<?php
+/**
+ * Date: 07.08.18
+ * @author Isaenko Alexey <info@oiplug.com>
+ */
+header('Content-Type: text/html; charset=utf-8');
+require '../db.php';
+require 'functions.php';
+//login();
+//logout();
+get_template('form');
+
+
+// eof
