220c73fdc14c2d3440beba99a43ad7a178258800
sh14 <sh-v@ya.ru>
2018-08-30 21:46:55 +0300

    правка стилей

---
+++ simple-site/seo-template.php
@@ -0,0 +1,16 @@
+<?php
+	$keword = 'важный текст';
+?>
+<h1>Важный текст на странице - заголовок</h1>
+
+<h2>В данной статье я расскажу о том, как правильно писать важный текст на странице.</h2>
+<p>Важный текст - вот о чем эта статья. В ней популярно объясняется как нужно писать текст и как отделять основной текст от второстепенного.</p>
+
+<h3>Подзаголовок</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium at commodi culpa minus sapiente vel voluptas?
+	Distinctio, doloremque dolores eaque eligendi est exercitationem illo minima molestiae optio pariatur quae ut.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium at commodi culpa minus sapiente vel voluptas?
+	Distinctio, doloremque dolores eaque eligendi est exercitationem illo minima molestiae optio pariatur quae ut.</p>
+<h3>Что должен включать в себя важный текст?</h3>
+вапывп
+.?
