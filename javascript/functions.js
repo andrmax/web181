f84c2f88e721a380652a0ec68678d63bc16aad65
mixdior <mixdior@gmail.com>
2018-09-16 19:55:19 +0300

    ДЗ от 21082018 Сделать функцию транслитерации При сохранении публикации формировать значение поля name с помощью транслитерации заголовка Сделать кнопку/ссылку для создания публикации Сделать так, чтобы в публикациях перенос строк переносил текст и при выводе в html

---
+++ javascript/functions.js
@@ -0,0 +1,65 @@
+/*let $ = 'text1\n' + "text2\n"
++ 'text3';
+alert( $  );*/
+'use strict';
+/*
+let massiv = [23,65];
+massiv.forEach(alert);
+*/
+/*
+
+alert(typeof x);
+if(undefined === typeof x ){
+	let x = 1;
+	alert(x);
+}
+
+let asd = 'sdf'*2;
+alert(asd);
+*/
+/*
+
+let z = [
+	3,
+	[ 'esfgs', 'Text' ]
+];
+console.log( z );
+
+let obj = {
+	key : 45,
+	name : 'Vasya',
+	age : 73,
+	scores : [ 5, 3, 5, 8 ],
+};
+console.log( obj );
+
+let myFirstVar    = 3; // эта переменная означает..
+let secondCoolVar = 4; // --//--
+let third         = 5;
+
+//let a=3,b=4,c=5;
+
+for ( let i = 0, a = 3; i < 10; i++ ) {
+	console.log( i + ') ' + i * a );
+}
+
+
+let num = '5';
+if(5 === num){
+	console.log( 'ok' );
+}
+*/
+
+/*
+for(let i = 0; i<10;i++){
+	console.log( i );
+}
+*/
+
+let mass = [ 4, 5, 3456, 75, 46 ];
+
+for ( let i in mass ) {
+	if ( mass.hasOwnProperty( i ) ) {
+		console.log( mass[ i ] );
+	}
+}
