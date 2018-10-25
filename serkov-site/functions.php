a06c2c1f3b69555c4b2abc9a32d77e1984459d6c
Zakhar <zakharserkov@me.com>
2018-10-25 18:57:09 +0300

    Сделал удаление сообщения о сохранении данных

--- serkov-site/functions.php
+++ serkov-site/functions.php
@@ -4,23 +4,62 @@
  * @author Isaenko Alexey <info@oiplug.com>
  */
 
+function fields_profile() {
+	$fields = array(
+		'fio'           => array(
+			'label'    => 'Имя и Фамилия',
+			'type'     => 'text',
+			'class'    => 'form__controll',
+			'required' => 1,
+		),
+		'bio'           => array(
+			'label' => 'Биография',
+			'type'  => 'text',
+			'class' => 'form__controll',
+		),
+		'birthday'      => array(
+			'label' => 'ДР',
+			'type'  => 'datetime-local',
+			'class' => 'form__controll',
+		),
+		'email'         => array(
+			'label'    => 'Email',
+			'type'     => 'text',
+			'class'    => 'form__controll',
+			'required' => 1,
+		),
+		'phone'         => array(
+			'label' => 'Телефон',
+			'type'  => 'text',
+			'class' => 'form__controll',
+		),
+		'meta[vk_link]' => array(
+			'label' => 'Ссылка VK',
+			'type'  => 'text',
+			'class' => 'form__controll',
+		),
+	);
 
-function get_user(){
+	return $fields;
+}
+
+
+function get_user() {
 
 
 	$data = array(
-		'image'=>'<img src="assets/images/photo.png" alt="" class="bio__image">',
-		'bio'=>'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dignissimos dolorum enim eveniet, exercitationem, hic ipsa iure laboriosam laborum maiores omnis, placeat possimus quibusdam quod veritatis! Doloribus eaque exercitationem voluptatem?',
-		'meta'=>array(
-			'dob'=>1920,
-			'address'=>'Moscow',
-			'phone'=>'+7 (903) 123 45 67',
-			'email'=>'minita@yandex.ru',
-			'site'=>'minita.ru',
-			'vk_link'=>'https://vk.com/minita.ru',
-			'vk_image'=>'',
-			'fb_link'=>'https://facebook.com/minitaru',
-			'fb_image'=>'',
+		'image' => '<img src="assets/images/photo.png" alt="" class="bio__image">',
+		'bio'   => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dignissimos dolorum enim eveniet, exercitationem, hic ipsa iure laboriosam laborum maiores omnis, placeat possimus quibusdam quod veritatis! Doloribus eaque exercitationem voluptatem?',
+		'meta'  => array(
+			'dob'      => 1920,
+			'address'  => 'Moscow',
+			'phone'    => '+7 (903) 123 45 67',
+			'email'    => 'minita@yandex.ru',
+			'site'     => 'minita.ru',
+			'vk_link'  => 'https://vk.com/minita.ru',
+			'vk_image' => '',
+			'fb_link'  => 'https://facebook.com/minitaru',
+			'fb_image' => '',
 		),
 	);
 
@@ -28,35 +67,38 @@ function get_user(){
 }
 
 
-function get_resume(){
+function get_resume() {
 
 
 	$fields = array(
 		array(
-			'start'=>'2015-03-01',
-			'end'=>'2015-12-01',
-			'position'=>'Механик',
-			'location'=>'Сызрань',
-			'description'=>'Работал в автосервисе, делал все.',
+			'start'       => '2015-03-01',
+			'end'         => '2015-12-01',
+			'position'    => 'Механик',
+			'location'    => 'Сызрань',
+			'description' => 'Работал в автосервисе, делал все.',
 		),
 		array(
-			'start'=>'2016-01-01',
-			'end'=>'2016-03-01',
-			'position'=>'Механик-электрик',
-			'location'=>'Саратов',
-			'description'=>'Выкручивал лампочки',
+			'start'       => '2016-01-01',
+			'end'         => '2016-03-01',
+			'position'    => 'Механик-электрик',
+			'location'    => 'Саратов',
+			'description' => 'Выкручивал лампочки',
 		),
 		array(
-			'start'=>'2016-04-01',
-			'end'=>'0000-00-00',
-			'position'=>'Механик-танцор',
-			'location'=>'Москва',
-			'description'=>'Ничего не делаю, но деньги получаю.',
+			'start'       => '2016-04-01',
+			'end'         => '0000-00-00',
+			'position'    => 'Механик-танцор',
+			'location'    => 'Москва',
+			'description' => 'Ничего не делаю, но деньги получаю.',
 		),
 	);
 
 	return $fields;
 }
+
+
+
 
 
 /*
@@ -87,6 +129,14 @@ function get_resume(){
  * id раздела
  * Название(имя раздела на русском языке)
  * Имя раздела, которое фигурирует в url'е
+ *
+ *
+ * ДЗ(23.10.2018):
+ * JS: Сделать так, чтобы после вывода сообщения об успешном или не успешном сохранении данных это сообщение пропадало черех Х секунд.
+ *
+ *
+ *
+ *
  */
 
 // eof
